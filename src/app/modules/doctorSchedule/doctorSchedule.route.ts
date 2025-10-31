a793482e945f4654c25720a971c0600d4a63c7ae
Arittro7 <arittrowpdev@gmail.com>
2025-11-01 00:07:30 +0600

    Doctor Schedule Task-2 code added

--- src/app/modules/doctorSchedule/doctorSchedule.route.ts
+++ src/app/modules/doctorSchedule/doctorSchedule.route.ts
@@ -12,5 +12,25 @@ router.post("/",
   validateRequest(doctorScheduleValidation.createDoctorScheduleValidationSchema),
   DoctorScheduleController.insertIntoDB )
 
+router.get(
+    '/',
+    auth(UserRole.ADMIN, UserRole.DOCTOR, UserRole.PATIENT),
+    DoctorScheduleController.getAllFromDB
+);
+
+router.get(
+    '/my-schedule',
+    auth(UserRole.DOCTOR),
+    DoctorScheduleController.getMySchedule
+)
+
+
+router.delete(
+    '/:id',
+    auth(UserRole.DOCTOR),
+    DoctorScheduleController.deleteFromDB
+);
+
+
 
 export const doctorScheduleRoutes = router
